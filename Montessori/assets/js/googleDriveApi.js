51466f19afdca112f309206b27dafed9927fba5f
Paul Ireifej <pireifej@gmail.com>
2019-10-22 22:17:38 -0400

    news editor
    move articles, save them, consolidate code

--- Montessori/assets/js/googleDriveApi.js
+++ Montessori/assets/js/googleDriveApi.js
@@ -6,14 +6,21 @@ var CLIENT_ID = "1047516465851-fsjve73h82ao14ltfiers341r3jcj9d3.apps.googleuserc
 var CLIENT_SECRET = "Uxs6ououVFzeFqC-XlNsAooq";
 
 // Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+						"https://docs.googleapis.com/$discovery/rest?version=v1"
+					];
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file';
+var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/documents.readonly';
 
 var authorizeButton = document.getElementById('authorize_button');
 var signoutButton = document.getElementById('signout_button');
+
+var files = [];
+var filesLength = 0;
+
+var NEWS_FOLDER_ID = null;
 
 //  On load, called to load the auth2 library and API client library
 function handleClientLoad() {
@@ -75,40 +82,15 @@ function handleClientLoad() {
 // get a file
 function getFile(fileId) {
 	console.log("get file...");
-	gapi.client.drive.files.get({
-		'fileId': fileId,
+	gapi.client.docs.documents.get({
+		'documentId': fileId,
 	}).then(function(response) {
-			console.log('Files:');
-			console.log(response.result.name);
+			addFileToArray(response);
 	});
 }
 
-// create a new folder
-function createNewFolder(folderName, callback) {
-var parentId = "1-0PK3OVb_DwzB8JPcX-ChMtovL8Wx2yW";//some parentId of a folder under which to create the new folder
-var fileMetadata = {
-  'name' : folderName,
-  'mimeType' : 'application/vnd.google-apps.folder',
-  'parents': parentId
-};
-gapi.client.drive.files.create({
-  resource: fileMetadata,
-}).then(function(response) {
-  switch(response.status){
-    case 200:
-      var file = response.result;
-	  callback(file.id);
-      console.log('Created Folder Id: ', file.id);
-      break;
-    default:
-      console.log('Error creating the folder, '+response);
-      break;
-    }
-});
-}
-
 // create new file
-function createNewWordDoc(folderId, title, content) {
+function createNewWordDoc(title, content) {
 var fileMetadata = {
     'mimeType': 'application/vnd.google-apps.document',
     'name': 'mrHanky'
@@ -121,7 +103,7 @@ var fileMetadata = {
   gapi.client.drive.files.create({
     name: newTitle,
 	mimeType: 'application/vnd.google-apps.document', //add the mimeType property
-    parents: folderId,
+    parents: NEWS_FOLDER_ID,
     resource: fileMetadata,
     media: media,
     params: {
@@ -130,9 +112,25 @@ var fileMetadata = {
     body: 'default2',
     fields: 'id'
   }).then(function(result) {
-    console.log('File create: ', result)
-    save(result.result.id, folderId, content).then(function(result) {
+    console.log('File create: ', result);
+    save(result.result.id, NEWS_FOLDER_ID, content).then(function(result) {
       console.log('File update', result)
+	  
+	  // move new file under news folder
+	  var body = {};
+	body.parents = [{ 'id': NEWS_FOLDER_ID }];
+	var request = gapi.client.request({
+  'path': 'drive/v2/files/'+result.result.id,
+  'method': 'PUT',
+  'params': {'uploadType': 'multipart', 'alt': 'json'}, 
+  'body': body
+});
+
+request.execute(function(resp) { 
+	console.log("file moved"); 
+	console.log(resp);
+	});
+	  
     })
   })
 }
@@ -150,24 +148,59 @@ function save(fileId, folderId, content) {
     })
 }
 
-// print files
+// print folders to find News folder
 function listFiles() {
-	console.log("list files...");
+	var folders = [];
+	console.log("List folders...");
 	gapi.client.drive.files.list({
-		'pageSize': 10,
+		'pageSize': 20,
+		'q': "mimeType = 'application/vnd.google-apps.folder' and trashed = false", 
 		'fields': "nextPageToken, files(id, name)"
-		}).then(function(response) {
-			console.log('Files:');
-			var files = response.result.files;
-			
-			if (files && files.length > 0) {
-				for (var i = 0; i < files.length; i++) {
-					var file = files[i];
-					console.log(file);
-					}
-			} else {
-				console.log('No files found.');
+	}).then(function(response) {
+		var newsFolderExists = false;
+		var folders = response.result.files;
+		if (folders && folders.length > 0) {
+			for (var i = 0; i < folders.length; i++) {
+				var folder = folders[i];
+				newsFolderExists = (folder.name == "News");
+				if (newsFolderExists) {
+					console.log("News folder exists already with ID " + folder.id);
+					NEWS_FOLDER_ID = folder.id;
+					break;
+				}
 			}
+			if (!newsFolderExists) {
+				console.log("News folder doesn't exist, so create it");
+				createNewFolder("News");
+			}
+		} else {
+			console.log('No folders found.');
+		}
+	});
+}
+
+// create a new folder
+function createNewFolder(folderName) {
+	var parentId = "1-0PK3OVb_DwzB8JPcX-ChMtovL8Wx2yW";//some parentId of a folder under which to create the new folder
+	var fileMetadata = {
+		'name' : folderName,
+		'mimeType' : 'application/vnd.google-apps.folder',
+		'parents': parentId
+	};
+
+	gapi.client.drive.files.create({
+		resource: fileMetadata,
+	}).then(function(response) {
+		switch(response.status){
+			case 200:
+				var file = response.result;
+				NEWS_FOLDER_ID = file.id;
+				console.log('Created Folder Id: ', file.id);
+				break;
+			default:
+				console.log('Error creating the folder, '+ response);
+				break;
+		}
 	});
 }
 
@@ -180,13 +213,14 @@ function listFolders(callback) {
 		  'q': "mimeType = 'application/vnd.google-apps.folder' and trashed = false",
           'fields': "nextPageToken, files(id, name)"
         }).then(function(response) {
-          console.log('Folders:');
+          console.log('Folderss:');
+		  console.log(response);
           var files = response.result.files;
           if (files && files.length > 0) {
             for (var i = 0; i < files.length; i++) {
               var file = files[i];
 			  var folder = { name: file.name, id: file.id };
-			  folders.push(folder);
+				
               console.log(file.name + ' (' + file.id + ')');
             }
 			callback(folders);
@@ -194,4 +228,35 @@ function listFolders(callback) {
             console.log('No folders found.');
           }
         });
+}
+
+function listAllFilesInFolder(folderId) {
+	  // list files under News folder
+	  var body = {};
+	body.parents = [{ 'id': folderId }];
+	var request = gapi.client.request({
+  'path': 'drive/v2/files/'+folderId + "/children",
+  'method': 'GET',
+  'body': body
+});
+
+request.execute(function(resp) { 
+	console.log("folder contains files ...");
+	filesLength = resp.items.length;
+	for (var i = 0; i < resp.items.length; i++) {
+		var item = resp.items[i];
+		console.log(item.id);
+		getFile(item.id, addFileToArray);
+	}
+	});
+}
+
+function addFileToArray(file) {
+	var newFile = JSON.parse(file.body);
+	files.push(newFile);	
+console.log(files.length);
+console.log(filesLength);
+	if (files.length == filesLength) {
+		displayNewsArticles(files);
+	}
 }
--- Montessori/assets/js/newsEditor.js
+++ Montessori/assets/js/newsEditor.js
@@ -1,57 +1,38 @@
-var NEWS_FOLDER_ID = null;
+
+
+$( document ).ready(function() {
+	$( "#datepicker" ).datepicker();
+});
 
 $( "#add-article" ).click(function() {
 	var date = $("#datepicker").val();
 	var title = $("#news-title").val();
 	var content = $("#content").val();
 	
-	addNewArticle(date, title, content);
-	
-	//console.log("saving article");
-	//getFile("12gQMUqWRc3twy6KNd9Cl8t19pS3nZrQo");
-	
-	// create News folder if doesn't already exist
-	listFolders(postFolderLoad);
-});
-
-function postCreateFolder(folderId) {
-	NEWS_FOLDER_ID = folderId;
-	console.log("postCreateFolder " + NEWS_FOLDER_ID);
-	
-	var date = $("#datepicker").val();
-	var title = $("#news-title").val();
-	var content = $("#content").val();
-	
 	var googleDriveContent = date + "\n" + title + "\n" + content;
 	
-	console.log(date, title, content);
-	createNewWordDoc(NEWS_FOLDER_ID, title, googleDriveContent);
-	
+	createNewWordDoc(title, googleDriveContent);
+	addNewArticle(date, title, content);
+		
 	$("#datepicker").val("");
 	$("#news-title").val("");
 	$("#content").val("");
-}
-
-function postFolderLoad(folders) {
-	var newsFolderExists = false;
-	for (var i = 0; i < folders.length; i++) {
-		var thisFolder = folders[i];
-		console.log(thisFolder);;
-		newsFolderExists = (thisFolder.name == "News");
-		if (newsFolderExists) { 
-			NEWS_FOLDER_ID = thisFolder.id;
-			postCreateFolder(NEWS_FOLDER_ID);
-			break;
-		}
-	}
-	if (!newsFolderExists) {
-		createNewFolder("News", postCreateFolder);
-	}
-}
-
-$( document ).ready(function() {
-	$( "#datepicker" ).datepicker();
 });
+
+function postListFiles(folderId) {
+	console.log("my news folder is ");
+	listAllFilesInFolder(folderId, displayNewsArticles);
+}
+
+function displayNewsArticles(files) {
+	console.log("displayNewsArticles");
+	
+	var newsArticles = [];
+	for (var i = 0; i < files.length; i++) {
+		var file = files[i];
+		console.log(file);
+	}
+}
 
 function addNewArticle(date, title, content) {
 	var articleHTML = "<article><header><h2>";
